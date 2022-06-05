fc6466815004451ebdfb396d9cc7783d03e3a9ec
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2022-06-05 18:02:08 +0200

    feat(showcase): display main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,41 @@ import '@mozaic-ds/mozaic-web-components/public/icons.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  toggleCheck = true;
+  checkboxValue = true;
+  inputNumber = 99;
+  opts = JSON.stringify([
+    {
+      text: 'Option 1',
+      value: 'option1',
+    },
+    {
+      text: 'Option 2',
+      value: 'option2',
+      disabled: true,
+    },
+    {
+      text: 'Option 3',
+      value: 'option3',
+    },
+    {
+      text: 'Option 4',
+      value: 'option4',
+    },
+  ]);
+  isLayerOpen = undefined;
+  handleToggle() {
+    this.toggleCheck = this.toggleCheck === undefined ? true : undefined;
+    alert('dont tickle me');
+  }
+  manageLayer() {
+    console.log(this.isLayerOpen);
+    this.isLayerOpen = !this.isLayerOpen;
+  }
+  closeLayer() {
+    console.log('Well done, Im closed now')
+  }
 }
